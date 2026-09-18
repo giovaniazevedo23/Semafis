b8c5c2ba47bef6e67932c4659d6964c405e9edca
giovaniazevedo23 <giovani.azevedo06@gmail.com>
2026-09-18 14:45:18 -0300

    Fix Express 5 wildcard routing error

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/process_payment', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Redirecionar todas as outras requisições para o index.html (React Router)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
